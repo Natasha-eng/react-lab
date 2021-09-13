c867477a4b2d681739ce622bba4981759d3104fa
Natasha-eng <natasha.shilova.89@gmail.com>
2021-09-13 12:17:55 +0300

    add debounce, handleKeyPress and productModalHandler functions,

--- src/components/search/SearchInput.tsx
+++ src/components/search/SearchInput.tsx
@@ -1,23 +1,43 @@
 import { AppRootState } from "@/app/storetype";
 import { GameType } from "@/types/types";
-import { ChangeEvent, useEffect, useState } from "react";
+import { ChangeEvent, KeyboardEvent, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { debounce } from "lodash";
 import { fetchGamesByNameThunkCreator } from "@/thunks/thunks";
 import { clearGamesAC } from "@/actions/actions";
+import Preloader from "@/elements/preloader/Preloader";
 import searchInputStyles from "./searchInput.module.css";
 
 function SearchInput(): JSX.Element {
   const dispatch = useDispatch();
-  const filteredGames = useSelector<AppRootState, Array<GameType>>((state) => state.filteredGames);
+  const filteredGames = useSelector<AppRootState, Array<GameType>>((state) => state.filteredGames.games);
+  const isFetching = useSelector<AppRootState, boolean>((state) => state.filteredGames.isFetching);
 
   const [name, setName] = useState<string>("");
-
-  // const db = dispatch(fetchGamesByNameThunkCreator(name));
+  const [search, setSearch] = useState(false);
 
   const searchByNameHandler = debounce((e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value === "") {
+      setSearch(false);
+    }
+    setSearch(true);
     setName(e.target.value);
   }, 1000);
+
+  const searchSlide = `${searchInputStyles.filteredGamesContainer} ${
+    search ? searchInputStyles.searchSlider : searchInputStyles.closeSearchSlider
+  }`;
+
+  const handleKeyPress = (e: KeyboardEvent<HTMLDivElement>) => {
+    if (e.code === "Enter") {
+      alert("got produck");
+    }
+  };
+
+  const productModalHandler = () => {
+    alert("got produck");
+  };
+
   useEffect(() => {
     if (name) {
       dispatch(fetchGamesByNameThunkCreator(name));
@@ -34,11 +54,17 @@ function SearchInput(): JSX.Element {
         placeholder="Search game..."
         onChange={searchByNameHandler}
       />
-      <div className={searchInputStyles.filteredGamesContainer}>
-        {filteredGames.map((g) => (
-          <div key={g.id}>{g.name}</div>
-        ))}
-      </div>
+      {isFetching ? (
+        <Preloader />
+      ) : (
+        <div className={searchSlide}>
+          {filteredGames.map((g) => (
+            <div key={g.id} onClick={productModalHandler} onKeyPress={handleKeyPress} role="button" tabIndex={0}>
+              {g.name}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
