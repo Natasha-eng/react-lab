d2f30c1291984d9be7d13c8543c091a4bc6e60d8
Natasha-eng <natasha.shilova.89@gmail.com>
2021-09-13 10:47:59 +0300

    create profile reducer, create profile component

---
+++ src/components/profile/Profile.tsx
@@ -0,0 +1,26 @@
+import { AppRootState } from "@/app/storetype";
+import { fetchProfileThunkCreator } from "@/thunks/thunks";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import main from "../../styles/main.module.css";
+
+interface IProfile {
+  isSignedIn: boolean;
+}
+
+export default function Profile(props: IProfile): JSX.Element {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const profile = useSelector<AppRootState, string>((state) => state.profile.profile);
+
+  if (!props.isSignedIn) {
+    history.push("/home");
+  }
+
+  useEffect(() => {
+    dispatch(fetchProfileThunkCreator());
+  }, []);
+
+  return <div className={main.container}>{profile}</div>;
+}
