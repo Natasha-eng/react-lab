b10cd45c36b4e4bc43289e499a00ea28525fdf75
Natasha-eng <natasha.shilova.89@gmail.com>
2021-09-30 13:03:05 +0300

    add loader img and styles

--- src/elements/preloader/Preloader.tsx
+++ src/elements/preloader/Preloader.tsx
@@ -1,10 +1,14 @@
-// import loader from "./../../../assets/images/loader";
+import loader from "../../assets/images/loading-icon.jpg";
+import main from "../../styles/main.module.css";
 
-export default function Preloader(): JSX.Element {
+interface IPreloader {
+  className: string;
+}
+
+export default function Preloader(props: IPreloader): JSX.Element {
   return (
-    <div>
-      {/* <img src={loader} /> */}
-      loading...
+    <div className={`${props.className} ${main.loader} `}>
+      <img src={loader} alt="loader" />
     </div>
   );
 }
