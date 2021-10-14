2e0dd3e0898b0c639585a72d06113a2e0204870c
Natasha-eng <natasha.shilova.89@gmail.com>
2021-10-14 17:52:40 +0300

    change IUser type, add type ICart

--- backend/src/readJsonFromFile.ts
+++ backend/src/readJsonFromFile.ts
@@ -1,11 +1,19 @@
 import * as fs from "fs/promises";
 
+export interface ICart {
+  gameId: number;
+  count: number | null;
+  orderDate: string;
+  price: number;
+}
+
 export interface IUser {
   id: number;
   login: string;
   email: string;
   profileDescription: string;
   password: string;
+  cart: ICart[];
   photo: string;
 }
 
