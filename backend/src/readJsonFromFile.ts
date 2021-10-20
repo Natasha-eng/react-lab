c2d1b4fe3d9c861889a41f7fd8ebc580faedeb85
Natasha-eng <natasha.shilova.89@gmail.com>
2021-10-20 13:14:25 +0300

    move types to folder types

--- backend/src/readJsonFromFile.ts
+++ backend/src/readJsonFromFile.ts
@@ -1,22 +1,5 @@
 import * as fs from "fs/promises";
 
-export interface ICart {
-  gameId: number;
-  count: number | null;
-  orderDate: string;
-  price: number;
-}
-
-export interface IUser {
-  id: number;
-  login: string;
-  email: string;
-  profileDescription: string;
-  password: string;
-  cart: ICart[];
-  photo: string;
-}
-
 const readJsonFromFile = (filePath: string): Promise<unknown> => fs.readFile(filePath, "utf8");
 
 export default readJsonFromFile;
--- backend/src/types/types.ts
+++ backend/src/types/types.ts
@@ -3,3 +3,36 @@ export interface IProfileData {
   email: string;
   profileDescription: string;
 }
+
+export interface IGame {
+  id: number;
+  name: string;
+  price: number;
+  date: string;
+  img: string;
+  category: string;
+  allowedAge: string;
+  genre: string;
+  description: string;
+}
+
+export interface ICart {
+  id: number;
+  name: string;
+  category: string;
+  amount: number;
+  orderDate: string;
+  price: number;
+  checked: boolean;
+}
+
+export interface IUser {
+  id: number;
+  login: string;
+  email: string;
+  profileDescription: string;
+  password: string;
+  cart: ICart[];
+  balance: number;
+  photo: string;
+}
