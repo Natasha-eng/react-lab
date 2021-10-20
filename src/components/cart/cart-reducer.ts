78c8fea946e2bc15fca96dc806a1798a111004f1
Natasha-eng <natasha.shilova.89@gmail.com>
2021-10-20 13:18:52 +0300

    create action clear cart, set carts

--- src/components/cart/cart-reducer.ts
+++ src/components/cart/cart-reducer.ts
@@ -1,17 +1,17 @@
 import { ActionsType } from "@/actions/actions";
 import { Types } from "@/constants/constants";
-import { CartGameType } from "@/types/types";
+import { ICart } from "@/types/types";
 
-const initialState: CartGameType[] = [];
+const initialState: ICart[] = [];
 
-export const cartReducer = (state = initialState, action: ActionsType): CartGameType[] => {
+export const cartReducer = (state = initialState, action: ActionsType): ICart[] => {
   switch (action.type) {
-    case Types.ADD_GAME:
-      return [{ ...action.gameData }, ...state];
+    case Types.SET_CARTS:
+      return action.updatedCart;
 
     case Types.CHANGE_GAME_STATUS:
       return state.map((cartGame) =>
-        cartGame.game.name === action.name ? { ...cartGame, checked: action.checked } : cartGame
+        cartGame.id === action.checkedGameId ? { ...cartGame, checked: action.checked } : cartGame
       );
 
     case Types.CHANGE_GAME_AMOUNT:
@@ -19,8 +19,11 @@ export const cartReducer = (state = initialState, action: ActionsType): CartGame
         cartGame.name === action.name ? { ...cartGame, amount: action.amount } : cartGame
       );
 
+    case Types.CLEAR_CART:
+      return initialState;
+
     case Types.REMOVE_GAME:
-      return state.filter((g: CartGameType) => !g.checked);
+      return state.filter((g) => !g.checked);
 
     default:
       return state;
