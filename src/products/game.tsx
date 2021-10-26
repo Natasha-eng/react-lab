b1231e4ed0dba3bdbe28768b9bfc9d191e574dab
Natasha-eng <natasha.shilova.89@gmail.com>
2021-10-26 12:23:58 +0300

    add modal for updating game

--- src/products/game.tsx
+++ src/products/game.tsx
@@ -1,19 +1,42 @@
-import { addGameThunkCreator } from "@/thunks/thunks";
+import { setMessageAC, updateGameAC } from "@/actions/actions";
+import { AppRootState } from "@/app/storetype";
+import UpdateGameModalContainer from "@/components/profile/updateGame/UpdateGameModalContainer";
+import { addGameThunkCreator, fetchGameThunkCreator } from "@/thunks/thunks";
 import { GameType } from "@/types/types";
-import { useDispatch } from "react-redux";
+import useLoader from "@/useLoader/useLoader";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import gameStyle from "./game.module.css";
 
 interface GamePropsType {
   game: GameType;
+  updateGame: (updatedGame: GameType) => void;
 }
 
 function Game(props: GamePropsType): JSX.Element {
+  const isAdmin = useSelector<AppRootState, boolean>((state) => state.profile.profile.isAdmin);
   const dispatch = useDispatch();
+  const [isModal, setIsModal] = useState(false);
 
   const addGameHandler = () => {
     const login = localStorage.getItem("signInLoginValue");
     login && dispatch(addGameThunkCreator(login, props.game.id));
   };
+
+  const toggleModal = () => {
+    setIsModal(!isModal);
+    dispatch(fetchGameThunkCreator(props.game.id, setLoaderHandler));
+    dispatch(setMessageAC(""));
+  };
+
+  const updateGameHandler = (updatedGame: GameType) => {
+    dispatch(updateGameAC(updatedGame));
+    props.updateGame(updatedGame);
+  };
+
+  const { setLoaderHandler, ComponentWithLoader } = useLoader(
+    <UpdateGameModalContainer toggleModal={toggleModal} updateGameHandler={updateGameHandler} />
+  );
 
   return (
     <div className={gameStyle.gameContainer} key={props.game.id}>
@@ -35,8 +58,15 @@ function Game(props: GamePropsType): JSX.Element {
           <button type="button" onClick={addGameHandler}>
             Add to cart
           </button>
+          {isAdmin && (
+            <button type="button" onClick={toggleModal}>
+              Edit
+            </button>
+          )}
         </div>
       </div>
+      {/* {isModal && <CreateGameModalContainer toggleModal={toggleModal} updateGameHandler={updateGameHandler} />} */}
+      {isModal && ComponentWithLoader}
     </div>
   );
 }
