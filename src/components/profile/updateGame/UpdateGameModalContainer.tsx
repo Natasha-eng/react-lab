50a21f30b0ae34840e7335322d82d2f85191ccfb
Natasha-eng <natasha.shilova.89@gmail.com>
2021-10-26 12:20:49 +0300

    created modal for updating game

---
+++ src/components/profile/updateGame/UpdateGameModalContainer.tsx
@@ -0,0 +1,16 @@
+import Modal from "@/components/modal/Modal";
+import { GameType } from "@/types/types";
+import UpdateGameModal from "./UpdateGameModal";
+
+interface ICreateGameModalr {
+  toggleModal: () => void;
+  updateGameHandler: (updatedGame: GameType) => void;
+}
+
+export default function UpdateGameModalContainer(props: ICreateGameModalr) {
+  return (
+    <Modal>
+      <UpdateGameModal toggleModal={props.toggleModal} updateGameHandler={props.updateGameHandler} />
+    </Modal>
+  );
+}
