ae6913225f553a341fb3cb43fa4476d9718795f1
Natasha-eng <natasha.shilova.89@gmail.com>
2021-10-26 12:33:05 +0300

    add action for user status

--- src/saga/saga.ts
+++ src/saga/saga.ts
@@ -1,17 +1,28 @@
-import { setErrorAC, setIsSignedInAC, setUserNameAC, signInSagaActionType } from "@/actions/actions";
+import {
+  setErrorAC,
+  setIsSignedInAC,
+  setUserBalanceAC,
+  setUserNameAC,
+  setUserStatusAC,
+  signInSagaActionType,
+} from "@/actions/actions";
 import { api } from "@/api/games-api";
 import { AxiosResponse } from "axios";
 import { call, put, StrictEffect, takeEvery } from "redux-saga/effects";
 
 function* signIn(action: signInSagaActionType) {
   try {
-    const user: AxiosResponse<{ status: number; name: string; errorMessage: string }> = yield call(
-      api.signIn,
-      action.payload.login,
-      action.payload.password
-    );
+    const user: AxiosResponse<{
+      status: number;
+      name: string;
+      balance: number;
+      isAdmin: boolean;
+      errorMessage: string;
+    }> = yield call(api.signIn, action.payload.login, action.payload.password);
     if (user.status === 201) {
       yield put(setUserNameAC(user.data.name));
+      yield put(setUserBalanceAC(user.data.balance));
+      yield put(setUserStatusAC(user.data.isAdmin));
       yield put(setIsSignedInAC(true));
     } else {
       yield put(setErrorAC(user.data.errorMessage));
