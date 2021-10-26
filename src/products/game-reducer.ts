5483fa5c5ea15488fb08fc797d49dc75311a346f
Natasha-eng <natasha.shilova.89@gmail.com>
2021-10-26 12:22:18 +0300

    create game-reducer

---
+++ src/products/game-reducer.ts
@@ -0,0 +1,26 @@
+import { ActionsType } from "@/actions/actions";
+import Types from "@/thunks/actiontTypes";
+import { GameType } from "@/types/types";
+
+const initialState: GameType = {
+  id: 0,
+  name: "",
+  price: 0,
+  description: "",
+  allowedAge: "",
+  data: "",
+  img: "",
+  category: "",
+  genre: "",
+};
+
+export const gameReducer = (state = initialState, action: ActionsType): GameType => {
+  switch (action.type) {
+    case Types.SET_GAME:
+      return action.game;
+    default:
+      return state;
+  }
+};
+
+export default gameReducer;
