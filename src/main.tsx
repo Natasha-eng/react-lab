f6f25817e110720a960411f17ece5a1f2af83a14
Natasha-eng <natasha.shilova.89@gmail.com>
2021-11-04 15:05:19 +0300

    Use react lazy

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,23 @@
 // watch: native intellisense and file-peek for aliases from jsconfig.json and with none-js files doesn't work: https://github.com/microsoft/TypeScript/issues/29334
-import { Component, StrictMode } from "react";
+// eslint-disable-next-line no-use-before-define
+import React, { Component, StrictMode } from "react";
 import ReactDom from "react-dom";
 import { BrowserRouter, Redirect, Route, RouteComponentProps, Switch } from "react-router-dom";
 import { Provider, useSelector } from "react-redux";
-import Home from "./components/home/Home";
 import Footer from "./components/footer/footer";
 import { store } from "./app/store";
-import About from "./components/about/About";
 import mainStyle from "./styles/main.module.css";
 import HeaderContainer from "./components/header/HeaderContainer";
 import ProtectedRoute from "./components/protectedRoute/ProtectedRoute";
-import Profile from "./components/profile/Profile";
 import { AppRootState } from "./app/storetype";
 import { path } from "./constants/constants";
-import Products from "./products/Products";
-import Cart from "./components/cart/Cart";
+import Preloader from "./elements/preloader/Preloader";
+
+const Home = React.lazy(() => import("./components/home/Home"));
+const About = React.lazy(() => import("./components/about/About"));
+const Profile = React.lazy(() => import("./components/profile/Profile"));
+const Products = React.lazy(() => import("./products/Products"));
+const Cart = React.lazy(() => import("./components/cart/Cart"));
 
 interface AppProps {
   nothing: boolean;
@@ -23,7 +26,7 @@ interface AppState {
   hasError: boolean;
 }
 
-function App() {
+const App = React.memo(() => {
   const isSignedIn = useSelector<AppRootState, boolean>((state) => state.auth.isSignedIn);
 
   return (
@@ -31,31 +34,33 @@ function App() {
       <HeaderContainer />
 
       <div className={mainStyle.container}>
-        <Switch>
-          <Route path={path.home} render={(routeProps: RouteComponentProps) => <Home {...routeProps} />} />
-          <ProtectedRoute path={path.products}>
-            <Products />
-          </ProtectedRoute>
+        <React.Suspense fallback={<Preloader className="" />}>
+          <Switch>
+            <Route path={path.home} render={(routeProps: RouteComponentProps) => <Home {...routeProps} />} />
+            <ProtectedRoute path={path.products}>
+              <Products />
+            </ProtectedRoute>
 
-          {/* <Route path="/products" render={() => <Products />} /> */}
-          {/* <Route path="/products/:category" render={() => <Products />} /> */}
-          {/* <Route path={path.about} render={() => <About />} /> */}
-          <ProtectedRoute path={path.about}>
-            <About />
-          </ProtectedRoute>
-          <Route path={path.profile} render={() => <Profile isSignedIn={isSignedIn} />} />
-          <ProtectedRoute path={path.cart}>
-            <Cart />
-          </ProtectedRoute>
-          <Redirect from="*" to={path.home} />
-          <Redirect to={path.profile} />
-        </Switch>
+            {/* <Route path="/products" render={() => <Products />} /> */}
+            {/* <Route path="/products/:category" render={() => <Products />} /> */}
+            {/* <Route path={path.about} render={() => <About />} /> */}
+            <ProtectedRoute path={path.about}>
+              <About />
+            </ProtectedRoute>
+            <Route path={path.profile} render={() => <Profile isSignedIn={isSignedIn} />} />
+            <ProtectedRoute path={path.cart}>
+              <Cart />
+            </ProtectedRoute>
+            <Redirect from="*" to={path.home} />
+            <Redirect to={path.profile} />
+          </Switch>
+        </React.Suspense>
       </div>
 
       <Footer />
     </div>
   );
-}
+});
 
 class AppContainer extends Component<AppProps, AppState> {
   ["constructor"]: typeof AppContainer;
