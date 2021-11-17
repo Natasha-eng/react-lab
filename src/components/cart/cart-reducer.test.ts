c9ff6efa3ed37d0ac8bf51edbb827c973d1766a5
Natasha-eng <natasha.shilova.89@gmail.com>
2021-11-17 15:44:55 +0300

    create fanctional test for cart-reducer

---
+++ src/components/cart/cart-reducer.test.ts
@@ -0,0 +1,53 @@
+import { ICart } from "app/types/types";
+import { cartReducer } from "./cart-reducer";
+import { changeGameStatusAC, setCartsAC } from "../../actions/actions";
+
+let state: ICart[];
+
+beforeEach(() => {
+  state = [
+    {
+      id: 1,
+      name: "Apex Legends",
+      category: "pc",
+      amount: 2,
+      orderDate: "2021-09-29T10:20:00Z",
+      price: 20,
+      checked: false,
+    },
+  ];
+});
+
+test("All the games in cart are desplayed", () => {
+  const newCarts = [
+    {
+      id: 1,
+      name: "Apex Legends",
+      category: "pc",
+      amount: 2,
+      orderDate: "2021-09-29T10:20:00Z",
+      price: 20,
+      checked: false,
+    },
+    {
+      id: 2,
+      name: "Rocket League",
+      category: "pc",
+      amount: 1,
+      orderDate: "2021-09-29T10:20:00Z",
+      price: 25,
+      checked: false,
+    },
+  ];
+
+  const newState = cartReducer(state, setCartsAC(newCarts));
+
+  expect(newState.length).toBe(2);
+});
+
+test("Game changes status", () => {
+  const newState = cartReducer(state, changeGameStatusAC(1, true));
+
+  expect(newState[0].checked).toBe(true);
+  expect(newState[0].id).toBe(1);
+});
