021f83fbd6565881223b1ac7413e78c15f0a63e5
Natasha-eng <natasha.shilova.89@gmail.com>
2021-11-23 12:07:23 +0300

    delete unudes data

--- src/components/header/HeaderContainer.tsx
+++ src/components/header/HeaderContainer.tsx
@@ -4,13 +4,6 @@ import { AppRootState } from "@/app/storetype";
 import { connect } from "react-redux";
 import Header from "./header";
 
-export const path = {
-  home: "/home",
-  products: "/products/:category?",
-  about: "/about",
-  profile: "/profile",
-};
-
 type mapStateToProps = {
   isSignedIn: boolean;
   userName: string;
